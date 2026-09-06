5c08583de5aa8a26b4551ca83689fbabcdd16bb1
soham <sohambasak17@gmail.com>
2026-09-06 21:02:42 +0530

    Fix Gradle test configuration

--- Backend/src/test/java/com/RealTimeCollaboration/RealTime/RealTimeApplicationTests.java
+++ Backend/src/test/java/com/RealTimeCollaboration/RealTime/RealTimeApplicationTests.java
@@ -2,12 +2,13 @@ package com.RealTimeCollaboration.RealTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RealTimeApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
